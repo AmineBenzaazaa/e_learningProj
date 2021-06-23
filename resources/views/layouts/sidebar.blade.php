1d09174c62908a1a995254c26a1c414c855bee18
AmineZaza <61782040+AmineZaza@users.noreply.github.com>
2021-06-23 16:02:37 +0100

    enter

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,10 +2,9 @@
 	<div class="leftbar-user active">
 		<a href="javascript: void(0);">
 			<img src="http://pn-learning-management-system-online.test/uploads/user_image/placeholder.png" alt="user-image" height="42" class="rounded-circle shadow-sm">
-						<span class="leftbar-user-name">admin admin</span>
+			<span class="leftbar-user-name">admin admin</span>
 		</a>
 	</div>
-
 	<!--- Sidemenu -->
 		<ul class="metismenu side-nav side-nav-light in">
 
@@ -26,43 +25,26 @@
 				</a>
 				<ul class="side-nav-second-level collapse" aria-expanded="false" style="height: 0px;">
 					<li class="">
-						<a href="http://pn-learning-management-system-online.test/admin/categories">Categories</a>
+						<a href="{{url('categories')}}">Categories</a>
 					</li>
 
 					<li class="">
-						<a href="http://pn-learning-management-system-online.test/admin/category_form/add_category">Add New Category</a>
+						<a href="{{url('add_category')}}">Add New Category</a>
 					</li>
 				</ul>
 			</li>
 
 			<li class="side-nav-item">
-				<a href="http://pn-learning-management-system-online.test/admin/courses" class="side-nav-link " aria-expanded="false">
+				<a href="{{url('courses')}}" class="side-nav-link " aria-expanded="false">
 					<i class="dripicons-archive"></i>
 					<span>Courses</span>
 				</a>
 			</li>
 			<li class="side-nav-item">
-				<a href="http://pn-learning-management-system-online.test/admin/users" class="side-nav-link " aria-expanded="false">
+				<a href="{{url('users')}}" class="side-nav-link " aria-expanded="false">
 					<i class="dripicons-user-group"></i>
 					<span>Students</span>
 				</a>
-			</li>
-
-			<li class="side-nav-item ">
-				<a href="javascript: void(0);" class="side-nav-link " aria-expanded="false">
-					<i class="dripicons-network-3"></i>
-					<span> Enrolment </span>
-					<span class="menu-arrow"></span>
-				</a>
-				<ul class="side-nav-second-level collapse" aria-expanded="false">
-					<li class="">
-						<a href="http://pn-learning-management-system-online.test/admin/enrol_history">Enrol History</a>
-					</li>
-
-					<li class="">
-						<a href="http://pn-learning-management-system-online.test/admin/enrol_student">Enrol A Student</a>
-					</li>
-				</ul>
 			</li>
 
 			<li class="side-nav-item">
@@ -72,17 +54,18 @@
 					<span class="menu-arrow"></span>
 				</a>
 				<ul class="side-nav-second-level collapse" aria-expanded="false">
-					<li class=""> <a href="http://pn-learning-management-system-online.test/admin/admin_revenue">Admin Revenue</a> </li>
-												<li class="">
-									<a href="http://pn-learning-management-system-online.test/admin/instructor_revenue">
-											Instructor Revenue <span class="badge badge-danger-lighten badge-pill">0</span>
-									</a>
-							</li>
-									</ul>
+					<li class=""> 
+                    <a href="{{url('admin_revenue')}} ">Admin Revenue</a> </li>
+							<li class="">
+								<a href="{{url('instructor_revenue')}} ">
+									Instructor Revenue <span class="badge badge-danger-lighten badge-pill">0</span>
+								</a>
+					</li>
+				</ul>
 			</li>
 
 			<li class="side-nav-item">
-				<a href="http://pn-learning-management-system-online.test/admin/message" class="side-nav-link " aria-expanded="false">
+				<a href="{{url('message')}}" class="side-nav-link " aria-expanded="false">
 					<i class="dripicons-message"></i>
 					<span>Message</span>
 				</a>
@@ -96,31 +79,25 @@
 			</a>
 			<ul class="side-nav-second-level collapse" aria-expanded="false">
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/system_settings">System Settings</a>
+					<a href="{{url('admin/system_settings')}}">System Settings</a>
 				</li>
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/frontend_settings">Website Settings</a>
+					<a href="{{url('admin/frontend_settings')}}">Website Settings</a>
 				</li>
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/payment_settings">Payment Settings</a>
+					<a href="{{url('admin/payment_settings')}}">Payment Settings</a>
 				</li>
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/instructor_settings">Instructor Settings</a>
+					<a href="{{url('admin/instructor_settings')}}">Instructor Settings</a>
 				</li>
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/manage_language">Language Settings</a>
+					<a href="{{url('admin/manage_language')}}">Language Settings</a>
 				</li>
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/smtp_settings">Smtp Settings</a>
+					<a href="{{url('admin/smtp_settings')}}">Smtp Settings</a>
 				</li>
 				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/about">About</a>
-				</li>
-				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/themes">Themes</a>
-				</li>
-				<li class="">
-					<a href="http://pn-learning-management-system-online.test/admin/mobile_app">Mobile App</a>
+					<a href="{{url('admin/about')}}">About</a>
 				</li>
 			</ul>
 		</li>
