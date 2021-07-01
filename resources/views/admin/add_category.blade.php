85ee53967737102049d1d8c03c89defa5bdd480b
AmineZaza <61782040+AmineZaza@users.noreply.github.com>
2021-07-01 16:18:43 +0100

    v1.4

--- resources/views/admin/add_category.blade.php
+++ resources/views/admin/add_category.blade.php
@@ -39,24 +39,6 @@
                                             <input type="text" class="form-control" id="name" name="name" required="">
                                         </div>
 
-                                        <div class="form-group" data-select2-id="4">
-                                            <label for="parent">Parent</label>
-                                            <select class="form-control select2 select2-hidden-accessible" data-toggle="select2" name="parent" id="parent" onchange="checkCategoryType(this.value)" data-select2-id="parent" tabindex="-1" aria-hidden="true">
-                                            <option value="0" data-select2-id="2">None</option>
-                                                                                                                                             <option value="1" data-select2-id="14">Math</option>
-                                        </select><span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="1" style="width: 530.455px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-parent-container"><span class="select2-selection__rendered" id="select2-parent-container" role="textbox" aria-readonly="true" title="None">None</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
-                                        </div>
-                                        
-                                        <div class="form-group" id="thumbnail-picker-area">
-                                            <label> Category Thumbnail <small>(The Image Size Should Be: 400 X 255)</small> </label>
-                                            <div class="input-group">
-                                                <div class="custom-file">
-                                                    <input type="file" class="custom-file-input" id="category_thumbnail" name="category_thumbnail" accept="image/*" onchange="changeTitleOfImageUploader(this)">
-                                                    <label class="custom-file-label" for="category_thumbnail">Choose Thumbnail</label>
-                                                </div>
-                                            </div>
-                                        </div>
-
                                         <button type="button" class="btn btn-primary" onclick="checkRequiredFields()">Submit</button>
                                     </form>
                                 </div>
